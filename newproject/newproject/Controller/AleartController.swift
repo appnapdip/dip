1babe6d2532d0907f23404e44a802eb4fd9e22e4
appnapdip <emdutt456@appnap.io>
2023-07-20 17:37:43 +0600

    Aleart button issue done

--- newproject/newproject/Controller/AleartController.swift
+++ newproject/newproject/Controller/AleartController.swift
@@ -15,14 +15,14 @@ class AleartViewController: ViewController  {
     
     var AleartTitle:UILabel = {
         let hexColor = UIColor(hex:"#323336")
-        let thisLabel = UILabel().label(title:"Remove Album Password", textColor:hexColor,fontStyle:UIFont(name: "Poppins-SemiBold", size:16),allignment:.center)
+        let thisLabel = UILabel().label(title:"Remove Album Password", textColor:hexColor,fontStyle:UIFont(name: "Poppins-SemiBold", size: .init(w:16)),allignment:.center)
         return thisLabel
         
     }()
     
     var  AleartSubTitle:UILabel = {
         let hexColor = UIColor(hex:"#5A5F73")
-        let thisLabel = UILabel().label(title:"This album will no longer required any password to be viewed", textColor:hexColor,fontStyle:UIFont(name: "Poppins-Regular", size:14),allignment:.center)
+        let thisLabel = UILabel().label(title:"This album will no longer required any password to be viewed", textColor:hexColor,fontStyle:UIFont(name: "Poppins-Regular", size: .init(w:14)),allignment:.center)
         return thisLabel
     }()
     
@@ -30,7 +30,7 @@ class AleartViewController: ViewController  {
     var AleartView: UIView = {
         let thisView = UIView()
         thisView.backgroundColor = .white
-        thisView.layer.cornerRadius = 16
+        thisView.layer.cornerRadius = .init(h:16)
         return thisView
     }()
     
@@ -38,7 +38,7 @@ class AleartViewController: ViewController  {
     
     lazy var CancelButton:UIButton = {
         let hexColor = UIColor(hex:"#5A5F73")
-        var thisButton = UIButton().button(title:"Cancel",titleColor:hexColor,font:UIFont(name:"Poppins-Medium", size:16)!,cornerRadius:.init(w:12))
+        var thisButton = UIButton().button(title:"Cancel",titleColor:hexColor,font:UIFont(name:"Poppins-Medium", size: .init(h:16))!,cornerRadius:.init(w:12))
         thisButton.layer.borderColor = UIColor(hex:"#323336").cgColor
         thisButton.layer.borderWidth = .init(h:1)
         return thisButton
@@ -47,7 +47,7 @@ class AleartViewController: ViewController  {
     
     lazy var RemoveButton:UIButton = {
         let hexColor = UIColor(hex:"#FFFFFF")
-        var thisButton = UIButton().button(title:"Remove",titleColor:hexColor,backgroundColor: UIColor(hex: "#FF4D4D"),font:UIFont(name:"Poppins-Medium", size:16)!,cornerRadius:.init(w:12))
+        var thisButton = UIButton().button(title:"Remove",titleColor:hexColor,backgroundColor: UIColor(hex: "#FF4D4D"),font:UIFont(name:"Poppins-Medium", size:.init(h:16))!,cornerRadius:.init(w:12))
         return thisButton
         
     }()
@@ -58,7 +58,7 @@ class AleartViewController: ViewController  {
         ButtonSStackView.axis = NSLayoutConstraint.Axis.horizontal
         ButtonSStackView.distribution = .fillEqually
         //thisPinStackView.alignment = .fill
-        ButtonSStackView.spacing = .init(w:18)
+        ButtonSStackView.spacing = UIdeviceSize.width * 0.04
         return ButtonSStackView
     }()
     
@@ -79,19 +79,17 @@ class AleartViewController: ViewController  {
         AleartView.addSubview(AleartTitle)
         AleartView.addSubview(AleartSubTitle)
         AleartView.addSubview(ButtonSStackView)
+        AleartView.centerY(inView: view)
         ButtonSStackView.addArrangedSubview(CancelButton)
         ButtonSStackView.addArrangedSubview(RemoveButton)
         
-        AleartView.anchorView(top:view.topAnchor,paddingTop: .init(h:355) , width:.init(w:334) , height:.init(h:187))
+        AleartView.anchorView(width:UIdeviceSize.width * 0.80 , height:UIdeviceSize.height * 0.20 )
         AleartView.centerX(inView:view)
         
-        AleartTitle.anchorView(top:AleartView.topAnchor,paddingTop: .init(h:20) , width:.init(w:208) , height:.init(h:16))
-        AleartTitle.centerX(inView:view)
+        AleartTitle.anchorView(top:AleartView.topAnchor,left:AleartView.leftAnchor ,right:AleartView.rightAnchor, paddingTop: .init(h:20), paddingLeft:.init(w:20), paddingRight:.init(w:20))
+        AleartSubTitle.anchorView(top:AleartTitle.bottomAnchor,left:AleartView.leftAnchor ,right:AleartView.rightAnchor, paddingTop: .init(h:10), paddingLeft:.init(w:20), paddingRight:.init(w:20))
         
-        AleartSubTitle.anchorView(top:AleartTitle.bottomAnchor,paddingTop: .init(h:10), width:.init(w:266), height:.init(h:35))
-        AleartSubTitle.centerX(inView:view)
-        
-        ButtonSStackView.anchorView(top:AleartSubTitle.bottomAnchor,paddingTop:.init(h:20),width:.init(w:278) , height:.init(h:48))
+        ButtonSStackView.anchorView(bottom:AleartView.bottomAnchor,paddingBottom:.init(h:20),width:UIdeviceSize.width*0.6715, height: UIdeviceSize.height*0.0536)
         ButtonSStackView.centerX(inView:view)
         
     }
