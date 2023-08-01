0f093c0eb9c4e6942d65b1a65a2c674f4f945513
appnapdip <emdutt456@appnap.io>
2023-08-01 11:49:16 +0600

    push done and folder allignment done

--- newproject/newproject/Controller/ViewController.swift
+++ newproject/newproject/Controller/ViewController.swift
@@ -147,7 +147,7 @@ class ViewController: UIViewController,Onboarding,PinDismiss,RemoveAleartView {
     }()
     
     lazy var AlbumUnderline:UIView = {
-       let  thisView = UIView()
+        let  thisView = UIView()
         thisView.backgroundColor = UIColor(hex: "#2EA7FF")
         thisView.isHidden = false
         return thisView
@@ -156,7 +156,7 @@ class ViewController: UIViewController,Onboarding,PinDismiss,RemoveAleartView {
     
     
     lazy var itemsUnderline:UIView = {
-       let  thisView = UIView()
+        let  thisView = UIView()
         thisView.backgroundColor = UIColor(hex:"#2EA7FF")
         thisView.isHidden = true
         return thisView
@@ -357,10 +357,8 @@ class ViewController: UIViewController,Onboarding,PinDismiss,RemoveAleartView {
     }
     
     @objc func pressOnSettings() {
-       let rootVC = ViewController()
-        let navVC = UINavigationController(rootViewController:rootVC)
-        let Svc = SettingsViewController()
-       navVC.pushViewController(Svc, animated:true)
+        let svc = SettingsViewController()
+        navigationController?.pushViewController(svc, animated:true)
     }
     
     
@@ -486,104 +484,6 @@ extension ViewController :  UICollectionViewDataSource, UICollectionViewDelegate
     }
 }
 
-// MARK: - BUTTONS CUSTOM CELL
-
-class buttonsCustomCell: UICollectionViewCell{
-    let allcatagoriesTiltle = UILabel().label(textColor:UIColor(hex:"#838BA7"),lines: 1 ,fontStyle:UIFont(name: "Poopins- Regular", size: .init(h:14)))
-    
-    override init(frame: CGRect) {
-        super.init(frame:frame)
-        contentView.addSubview(allcatagoriesTiltle)
-        backgroundColor = UIColor(hex: "#FFFFFF")
-        layer.cornerRadius = .init(h:12)
-        layer.borderColor = UIColor(hex:"#D6D8E2").cgColor
-        layer.borderWidth = .init(h:0.5)
-        loadUI()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK: - ButtonscustomCell LoadUI
-    
-    private func loadUI() {
-        allcatagoriesTiltle.anchorView(top:topAnchor,left:leftAnchor,bottom:bottomAnchor,right:rightAnchor,paddingTop:.init(h:8),paddingLeft: .init(h:16),paddingBottom:.init(h:8),paddingRight:.init(w:16))
-        contentView.anchorView(width:UIdeviceSize.width)
-    }
-}
-
-// MARK: - ALBUMS CUSTOM CELL
-
-class albumsCustomCell: UICollectionViewCell{
-    
-    let albumsTitle = UILabel().label(textColor: UIColor(hex:"#323336"),fontStyle:UIFont(name: "Poppins-Medium", size:.init(h:16)),allignment:.left)
-    let albumsImage = UIImageView().Image(contantMode:.scaleAspectFit)
-    let albumsizeTitle = UILabel().label(textColor: UIColor(hex:"#838BA7"),fontStyle:UIFont(name: "Poppins-Regular", size:.init(h:14)),allignment:.left)
-    let albumIcon = UILabel().label(fontStyle:UIFont(name: "Poppins-Medium", size:.init(h:28)),allignment:.left)
-    
-    override init(frame: CGRect) {
-        super.init(frame:frame)
-        contentView.addSubview(albumsImage)
-        albumsImage.addSubview(albumIcon)
-        albumsImage.addSubview(albumsTitle)
-        albumsImage.addSubview(albumsizeTitle)
-        albumsImage.image = UIImage(named:"Vector")
-       
-        
-        loadUI()
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK: - AlbumCustomCell LoadUI
-    
-    private func loadUI() {
-        albumsImage.anchorView(top:topAnchor,left:leftAnchor,bottom:bottomAnchor,right:rightAnchor)
-        albumIcon.anchorView(top:albumsImage.topAnchor,left:albumsImage.leftAnchor,paddingTop:.init(h:20),paddingLeft:.init(w:22))
-        albumsizeTitle.anchorView(left:albumsImage.leftAnchor,bottom:albumsImage.bottomAnchor,paddingLeft:.init(w:22),paddingBottom:.init(h:20))
-        albumsTitle.anchorView(left:albumsImage.leftAnchor, bottom:albumsizeTitle.topAnchor,paddingLeft:.init(w:22), paddingBottom:.init(h:6))
-        
-    }
-    
-}
-
-//MARK: - ITEMS IMAGE CELL
-
-
-class itemImageCell:UICollectionViewCell {
-    
-    let itemImageView = UIImageView().Image(contantMode:.scaleAspectFit)
-    
-    override init(frame: CGRect) {
-        super.init(frame:frame)
-        contentView.addSubview(itemImageView)
-        loadUI()
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-     func loadUI() {
-         itemImageView.anchorView(top:topAnchor,left:leftAnchor,bottom:bottomAnchor,right:rightAnchor)
-         
-         
-        
-    }
-    
-    
-}
-
-
-
-// MARK: - Create Extension
-
-
 // MARK: - Create Array Of Function
 
 func albums() ->[Album] {
@@ -594,8 +494,6 @@ func albums() ->[Album] {
     let album5 = Album(id: "", icon: "🗄", name: "Travel", size:"\(200) items・\(200) mb", isLocked:true, items: [])
     return [album1,album2,album3,album4,album5]
 }
-
-
 
 func Items() -> [Item] {
     let item1 = Item(id: "", name: "", type: "", Size: 0, resource: "Rectangle 10")
---
+++ newproject/newproject/View/Cells/Cell.swift
@@ -0,0 +1,99 @@
+//
+//  Cell.swift
+//  newproject
+//
+//  Created by New User on 8/1/23.
+//
+
+import Foundation
+import UIKit
+
+
+
+// MARK: - BUTTONS CUSTOM CELL
+
+class buttonsCustomCell: UICollectionViewCell{
+    let allcatagoriesTiltle = UILabel().label(textColor:UIColor(hex:"#838BA7"),lines: 1 ,fontStyle:UIFont(name: "Poopins- Regular", size: .init(h:14)))
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        contentView.addSubview(allcatagoriesTiltle)
+        backgroundColor = UIColor(hex: "#FFFFFF")
+        layer.cornerRadius = .init(h:12)
+        layer.borderColor = UIColor(hex:"#D6D8E2").cgColor
+        layer.borderWidth = .init(h:0.5)
+        loadUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - ButtonscustomCell LoadUI
+    
+    private func loadUI() {
+        allcatagoriesTiltle.anchorView(top:topAnchor,left:leftAnchor,bottom:bottomAnchor,right:rightAnchor,paddingTop:.init(h:8),paddingLeft: .init(h:16),paddingBottom:.init(h:8),paddingRight:.init(w:16))
+        contentView.anchorView(width:UIdeviceSize.width)
+    }
+}
+
+// MARK: - ALBUMS CUSTOM CELL
+
+class albumsCustomCell: UICollectionViewCell{
+    
+    let albumsTitle = UILabel().label(textColor: UIColor(hex:"#323336"),fontStyle:UIFont(name: "Poppins-Medium", size:.init(h:16)),allignment:.left)
+    let albumsImage = UIImageView().Image(contantMode:.scaleAspectFit)
+    let albumsizeTitle = UILabel().label(textColor: UIColor(hex:"#838BA7"),fontStyle:UIFont(name: "Poppins-Regular", size:.init(h:14)),allignment:.left)
+    let albumIcon = UILabel().label(fontStyle:UIFont(name: "Poppins-Medium", size:.init(h:28)),allignment:.left)
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        contentView.addSubview(albumsImage)
+        albumsImage.addSubview(albumIcon)
+        albumsImage.addSubview(albumsTitle)
+        albumsImage.addSubview(albumsizeTitle)
+        albumsImage.image = UIImage(named:"Vector")
+        
+        
+        loadUI()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - AlbumCustomCell LoadUI
+    
+    private func loadUI() {
+        albumsImage.anchorView(top:topAnchor,left:leftAnchor,bottom:bottomAnchor,right:rightAnchor)
+        albumIcon.anchorView(top:albumsImage.topAnchor,left:albumsImage.leftAnchor,paddingTop:.init(h:20),paddingLeft:.init(w:22))
+        albumsizeTitle.anchorView(left:albumsImage.leftAnchor,bottom:albumsImage.bottomAnchor,paddingLeft:.init(w:22),paddingBottom:.init(h:20))
+        albumsTitle.anchorView(left:albumsImage.leftAnchor, bottom:albumsizeTitle.topAnchor,paddingLeft:.init(w:22), paddingBottom:.init(h:6))
+        
+    }
+    
+}
+
+//MARK: - ITEMS IMAGE CELL
+
+class itemImageCell:UICollectionViewCell {
+    
+    let itemImageView = UIImageView().Image(contantMode:.scaleAspectFit)
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        contentView.addSubview(itemImageView)
+        loadUI()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func loadUI() {
+        itemImageView.anchorView(top:topAnchor,left:leftAnchor,bottom:bottomAnchor,right:rightAnchor)
+    }
+}
+
